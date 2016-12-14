25e1a83fc9a43ff2ed698324dc913bf4da675d4c
Yuki <tigers24.backfrip.yuki1117@gmail.com>
2016-12-14 13:10:37 +0000

    バリデーション作成

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,13 @@
 class Customer < ActiveRecord::Base
+    validates :family_name,
+        presence: true, length: { maximum: 20 }
+    calidates :given_name,
+        presence: true, length { maximum: 20 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/index
+    validates :email
+        presence :true, format: { with: VALID_EMAIL_REGEX }, uniquness: true
+    def full_name
+        # return family_name + given_nameの略
+        family_name + given_name
+    end
 end
